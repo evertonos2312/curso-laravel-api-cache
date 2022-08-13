f309a07eb5e877319614f85237ba63ef505b397f
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2022-08-13 15:57:33 -0300

    Courses

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
     return response()->json(['message' => 'ok']);
 });
+
+Route::get('/courses', [CourseController::class, 'index']);
